a429d2d004e6f0e7017dae164d90743f34419f03
Evan Phillip Wang <epw268@nyu.edu>
2024-02-11 23:45:07 -0500

    Update mergeKsrtedLL.py

--- mergeKsrtedLL.py
+++ mergeKsrtedLL.py
@@ -1,4 +1,6 @@
+import math
 # Definition for singly-linked list.
+
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
@@ -7,7 +9,14 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         # different sizes
         # being disprop
+        lists   =   lists.sorted(lists, keys    =   lambda x: x[0]) #O(n log n)
         minimum = min([a[0].val for a in lists])
+        layers  =   math.log(len(lists))+1    #2^k < len(lists)
+        for a in range(len(lists)//2):#odd indexes
+            index = a*2 # sort pairs then delete the slot after with del() or remove()
+            lists[a]    =   sorted(lists[index],lists[a+1])
+        #mergesort
+
         curr = ListNode(minimum, None)
 
         full = [1 for a in lists]
