bfc1ea6ecb374ba612815667288e888bfb659dc4
Evan Phillip Wang <epw268@nyu.edu>
2024-02-12 23:38:14 -0500

    added a base case

--- mergeKsrtedLL.py
+++ mergeKsrtedLL.py
@@ -9,20 +9,26 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         # different sizes
         # being disprop
-        lists   =   lists.sorted(lists, keys    =   lambda x: x[0]) #O(n log n)
-        minimum = min([a[0].val for a in lists])
-        layers  =   math.log(len(lists))+1    #2^k < len(lists)
-        for a in range(len(lists)//2):#odd indexes
-            index = a*2 # sort pairs then delete the slot after with del() or remove()
-            lists[a]    =   sorted(lists[index],lists[a+1])
-        #mergesort
+        if  len(lists)  <=  2:
+            if len(lists)   ==  2:
+                return  lists.sorted(lists[0]   +   lists[1])
+            else:
+                return lists[0]
+        else:
+            lists   =   lists.sorted(lists, keys    =   lambda x: x[0]) #O(n log n)
+            minimum = min([a[0].val for a in lists])
+            layers  =   math.log(len(lists))+1    #2^k < len(lists)
+            for a in range(len(lists)//2):#odd indexes
+                index = a*2 # sort pairs then delete the slot after with del() or remove()
+                lists[a]    =   sorted(lists[index],lists[a+1])
+            #mergesort
 
-        curr = ListNode(minimum, None)
+            curr = ListNode(minimum, None)
 
-        full = [1 for a in lists]
-        for a   == 1 in full:
+            full = [1 for a in lists]
+            for a   == 1 in full:
 
-        return full
+            return full
 
-        def shorten(self, list: List[Optional[ListNode]]) -> Optional[ListNode]:
+        #def shorten(self, list: List[Optional[ListNode]]) -> Optional[ListNode]:
             list
