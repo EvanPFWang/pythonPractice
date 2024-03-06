a0622ed800c8384af2bd6e0f0089913fe5911eb0
Lam Nguyen <77522931+EvanPFWang@users.noreply.github.com>
2024-03-05 20:05:41 -0500

    Update normal_bStrap.py

--- normal_bStrap.py
+++ normal_bStrap.py
@@ -1,6 +1,7 @@
-#import mathplotlib.plotly as plt
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 import torch
-# number of random variables X_i
+import matplotlib.pyplot as plt
 
 
 
@@ -16,7 +17,7 @@ b = 10000
 # probability of success which we hope to estimate
 p = 0.7
 
-# draw X_i ∼ Bernoulli(p)
+# draw X_i ∼ Bernoulli(p) observations
 rvX = torch.distributions.Bernoulli(p).sample((n,))
 #print(rvX)
 
@@ -34,11 +35,25 @@ T = 1/n *   rvX_empirical.sum(0)
 T_mean = T.mean()
 
 v_boot  =   torch.mean((T   -   T_mean)**2)
-v_true  =   p(1-p)/n
+v_true  =   p*(1-p)/n
 def norm_pdf(data, mean, std):
+    const   =   (1 / (std * torch.sqrt(torch.tensor(2 * torch.pi))))
+    return const    *   torch.exp(-0.5 * ((data - mean) / std) ** 2)
 
-# compute bootstrap variance
-#v_boot = sum((T .- T_mean).^2) / B
+standard_err    =   torch.sqrt(torch.tensor(v_true))
+dom  =   torch.linspace(p-5*standard_err,p+5*standard_err,n)
 
-# compute true variance of sample mean T_n for comparison
-v_true = p*(1-p) / n
+
+v_boot_pdf = norm_pdf(dom,T_mean,torch.sqrt(v_boot))
+v_true_pdf = norm_pdf(dom, T_mean,  torch.sqrt(torch.tensor(v_true)))
+
+
+plt.figure(figsize  =   (12,9))
+plt.title(f"n = {n}, B = {b}, True Var(T_n) = {v_true:.2e}, Bootstrap Var(T_n) = {v_boot:.2e}'")
+plt.hist(T.numpy(), bins = 50, density = True,alpha    =   0.1, label  =   "Bootstrap Samples")
+plt.plot(dom, v_boot_pdf, "b--",    label = "Boot Var PDF")
+plt.plot(dom,v_true_pdf,"r--",  label = "True Var PDF")
+plt.xlabel("Sample Mean")
+plt.ylabel("Density")
+plt.legend()
+plt.show()
