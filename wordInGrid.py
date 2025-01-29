bf6b18fd28b5f69f6ea97ac05f545985af650b7a
EvanPFWang <77522931+EvanPFWang@users.noreply.github.com>
2025-01-28 23:36:34 -0500

    Update wordInGrid.py

--- wordInGrid.py
+++ wordInGrid.py
@@ -16,11 +16,24 @@ def currentSearch(matrix,word,x,y,wordIndexOn):
                      currentSearch(matrix,word,x,y+1,wordIndexOn+1),
                      currentSearch(matrix,word,x+1,y,wordIndexOn+1))
         matrix[x][y] = temp
-        return  delta
+        return  deltaBool
     return False
 
 
 
+
+def gen(n):
+    i = 0
+    while i < n:
+        yield i
+        i += 1
+
+
+
+        # If first letter matches, then recur and check
+        if mat[i][j] == word[0]:
+            if findMatch(mat, word, i, j, 0):
+                return True
 
 
 def wordInGrid(matrix,word):
@@ -28,3 +41,13 @@ def wordInGrid(matrix,word):
 
     n = len(matrix)
     m = len(matrix[0])
+
+    if wordLen  <   n*m:
+        return False
+    for i in gen(n):
+        for j in gen(m):
+            if  matrix[i][j] == word[0]:
+                if findMatch(matrix, word, i, j, 0):
+                    return True
+    return False
+
