ba851dc87d5e28ac738ae00222ae73e7f2194372
EvanPFWang <77522931+EvanPFWang@users.noreply.github.com>
2025-01-28 23:46:21 -0500

    Update wordInGrid.py

--- wordInGrid.py
+++ wordInGrid.py
@@ -1,53 +1,47 @@
-def currentSearch(matrix,word,x,y,wordIndexOn):
-    n   = len(matrix)
-    m   = len(matrix[0])
-    wordLen = len(word)
-    if wordIndexOn == wordLen:
-        return True
-    if  x<0 or x>=n or y<0 or y>=m:
+class   Solution:
+    def currentSearch(self,matrix,word,x,y,wordIndexOn):
+        n   = len(matrix)
+        m   = len(matrix[0])
+        wordLen = len(word)
+        if wordIndexOn == wordLen:
+            return True
+        if  x<0 or x>=n or y<0 or y>=m:
+            return False
+        cursor  =   matrix[x][y]
+        if cursor == word[wordIndexOn]:
+
+            temp    = matrix[x][y]
+            matrix[x][y] = "~"
+            deltaBool   =   (self.currentSearch(matrix,word,x-1,y,wordIndexOn+1) or
+                        self.currentSearch(matrix,word,x,y-1,wordIndexOn+1) or
+                        self.currentSearch(matrix,word,x,y+1,wordIndexOn+1) or
+                        self.currentSearch(matrix,word,x+1,y,wordIndexOn+1))
+            matrix[x][y] = temp
+            return  deltaBool
         return False
-    cursor  =   matrix[x][y]
-    if cursor == word[wordIndexOn]:
-
-        temp    = matrix[x][y]
-        matrix[x][y] = "~"
-        delta   =   (currentSearch(matrix,word,x-1,y,wordIndexOn+1),
-                     currentSearch(matrix,word,x,y-1,wordIndexOn+1),
-                     currentSearch(matrix,word,x,y+1,wordIndexOn+1),
-                     currentSearch(matrix,word,x+1,y,wordIndexOn+1))
-        matrix[x][y] = temp
-        return  deltaBool
-    return False
 
 
 
 
-def gen(n):
-    i = 0
-    while i < n:
-        yield i
-        i += 1
+    def gen(self,n):
+        i = 0
+        while i < n:
+            yield i
+            i += 1
 
 
+    def exist(self, matrix: List[List[str]], word: str) -> bool:
+        wordLen = len(word)
+        n = len(matrix)
+        m = len(matrix[0])
 
-        # If first letter matches, then recur and check
-        if mat[i][j] == word[0]:
-            if findMatch(mat, word, i, j, 0):
-                return True
+        if wordLen > n * m:
+            return False
 
-
-def wordInGrid(matrix,word):
-    wordLen = len(word)
-
-    n = len(matrix)
-    m = len(matrix[0])
-
-    if wordLen  <   n*m:
+        for i in self.gen(n):
+            for j in self.gen(m):
+                if matrix[i][j] == word[0]:
+                    if self.currentSearch(matrix, word, i, j, 0):
+                        return True
         return False
-    for i in gen(n):
-        for j in gen(m):
-            if  matrix[i][j] == word[0]:
-                if findMatch(matrix, word, i, j, 0):
-                    return True
-    return False
 
